6dedee138e203eff623a0d5ca00a58f2a417b31a
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-04-11 16:03:34 +0530

    CRUD done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Authetication from './components/Authetication'
 import {db, auth} from "./config/firebase"
 import { useEffect, useState } from 'react'
-import { getDocs, collection, addDoc } from 'firebase/firestore'
+import { getDocs, collection, addDoc, deleteDoc, doc } from 'firebase/firestore'
 import './App.css'
 
 
@@ -14,6 +14,7 @@ function App() {
   const [studentBranch,setStudentBranch] = useState("")
   const [studentUSN,setStudentUSN] = useState("")
   const [studentPromoted,setPromoted] = useState("")
+  const [studentUpdatedPromotion,setUpdatedPromotion] = useState("")
   
 
   const studentCollection = collection(db,"Students")
@@ -23,7 +24,6 @@ function App() {
     try {
       const data = await getDocs(studentCollection)
       const studentList = data.docs.map((doc) => ({...doc.data(), id:doc.id}))
-      console.log(studentList)
       setStudentList(studentList)
     } catch (error) {
       console.error(error)
@@ -40,11 +40,24 @@ function App() {
         {name:studentName,
          USN:studentUSN,
          branch:studentBranch,
-         isPromoted:studentPromoted});
+         isPromoted:studentPromoted,
+         userID: auth?.currentUser?.uid});
       getStudents()
     } catch (error) {
       console.error(error)
     }
+  }
+
+  const deleteStudent = async (id) => {
+    
+    try {
+      const studentDoc = doc(db,"Students",id)
+      await deleteDoc(studentDoc)
+      getStudents()
+    } catch (error) {
+      console.error(error)
+    }
+
   }
 
   return (
@@ -53,8 +66,8 @@ function App() {
 
       <Authetication/>
 
-      <div className='CRUD'>
-        <h2>--  FIREBASE CRUD DEMO  --</h2>
+      <div style={{marginBottom:"7rem"}} className='CRUD'>
+        <h2>--  CRUD IN FIREBASE  --</h2>
         <div className='add-student'>
           <h3>ADD STUDENT: </h3>
           <input onChange={(e) => setStudentName(e.target.value)} placeholder='Name'/>
@@ -63,19 +76,25 @@ function App() {
           <label><input onChange={(e) => setPromoted(e.target.checked)} type='checkbox'/>PROMOTED</label>
           <button 
             style={{display:"block",margin:"2rem auto"}} 
-            className='signout'
+            className='.g-signin'
             onClick={addStudent}>ADD</button>
         </div>
         <div className='student-list'>
         <h3>STUDENT LIST:</h3>
         {studentList.map(student => {
           return(
-              <h3  key={student.id} style={{color: student.isPromoted ? "green" : "red"}}>
+            <div key={student.id}>
+              <h3   style={{display:"inline", color: student.isPromoted ? "green" : "red"}}>
                 {student.name} | {student.USN} | {student.branch}
               </h3>
+              <button onClick={() => deleteStudent(student.id)} className='btn'>🚫</button>
+            </div>
           )
         })}
         </div>
+      </div>
+      <div style={{marginBottom:"7rem"}} className='STORAGE'>
+        
       </div>
     </div>
   )

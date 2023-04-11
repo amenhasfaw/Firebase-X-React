57f4c7bd4490147b15f45b186c1b7a5dbd19a918
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-04-11 12:52:39 +0530

    Email Auth added]

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import Authetication from './components/Authetication'
 import './App.css'
+
 
 function App() {
 
   return (
     <div>
       <h1>FIREBASE X REACT</h1>
+      <Authetication/>
     </div>
   )
 }
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {getAuth} from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBztExy8p4YzVjMqA2DyBgLUDHJKdIECxQ",
   authDomain: "fir-x-react-d7d96.firebaseapp.com",
@@ -16,6 +12,7 @@ const firebaseConfig = {
   measurementId: "G-W0N7S0VXN7"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const auth = getAuth(app);
